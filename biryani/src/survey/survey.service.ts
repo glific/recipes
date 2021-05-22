8b85bbd9491db18ff6a970de0bdd21481b4a53cd
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-05-22 17:28:44 +0100

    now we can fetch data from sample sheet

--- biryani/src/survey/survey.service.ts
+++ biryani/src/survey/survey.service.ts
@@ -1,8 +1,52 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class SurveyService {
-  save(response) {
+  constructor(private configService: ConfigService) {}
+  async save(response) {
+    const keyFilename = this.configService.get<string>(
+      'GOOGLE_APPLICATION_KEY',
+    );
+    const sheets = require('@googleapis/sheets');
+
+    const auth = new sheets.auth.GoogleAuth({
+      keyFilename,
+      // Scopes can be specified either as an array or as a single, space-delimited string.
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    });
+    const authClient = await auth.getClient();
+
+    this.listMajors(authClient);
+
     return 'Hello';
+  }
+
+  /**
+   * Prints the names and majors of students in a sample spreadsheet:
+   * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+   * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+   */
+  listMajors(auth) {
+    const { google } = require('googleapis');
+    const sheets = google.sheets({ version: 'v4', auth });
+    sheets.spreadsheets.values.get(
+      {
+        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+        range: 'Class Data!A2:E',
+      },
+      (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const rows = res.data.values;
+        if (rows.length) {
+          console.log('Name, Major:');
+          // Print columns A and E, which correspond to indices 0 and 4.
+          rows.map((row) => {
+            console.log(`${row[0]}, ${row[4]}`);
+          });
+        } else {
+          console.log('No data found.');
+        }
+      },
+    );
   }
 }
