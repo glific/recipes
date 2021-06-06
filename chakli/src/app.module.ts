c53b9f24a1e9cd611e8ec68444fea993eb18d879
Kurund Jalmi <kurund.jalmi@gmail.com>
2021-06-06 16:37:54 +0100

    add mailchimp, now subscriptions are working

--- chakli/src/app.module.ts
+++ chakli/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ListModule } from './list/list.module';
 
 @Module({
-  imports: [ListModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ListModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- chakli/src/list/list.service.ts
+++ chakli/src/list/list.service.ts
@@ -1,12 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ListService {
+  private mailchimpClient;
+
+  constructor(private readonly configService: ConfigService) {
+    this.mailchimpClient = require('@mailchimp/mailchimp_marketing');
+
+    this.mailchimpClient.setConfig({
+      apiKey: this.configService.get<string>('MAILCHIMP_API_KEY'),
+      server: this.configService.get<string>('MAILCHIMP_SERVER_PREFIX'),
+    });
+  }
+
+  async managaeSubscription(data, status) {
+    const listId = this.configService.get<string>('MAILCHIMP_LIST_ID');
+
+    const email_address = data.email_address;
+    const md5 = require('md5');
+    const subscriber_hash = md5(email_address.toLowerCase());
+
+    await this.mailchimpClient.lists.setListMember(listId, subscriber_hash, {
+      email_address,
+      status_if_new: status,
+      status,
+    });
+  }
+
   subscribe(response) {
+    this.managaeSubscription(response, 'subscribed');
     return { message: 'Subscribed successfully.' };
   }
 
   unsubscribe(response) {
+    this.managaeSubscription(response, 'unsubscribed');
     return { message: 'Unsubscribed successfully.' };
   }
 }
